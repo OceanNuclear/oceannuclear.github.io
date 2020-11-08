846daf525575fd058d6993fe9e703fb3571da388
OceanNuclear <annoyingamygdala@gmail.com>
2020-11-08 11:07:51 +0000

    updated website's everything except footers

--- README.txt
+++ README.txt
@@ -1,15 +1,8 @@
-Todo (Ocean):
-
+# Todo (Ocean):
 - Make the footers (emailing) work; and then sync across the pages
-- create mini-index for showcase and visualization
-- create icon at the address bar
-- Use following photos to represent "landscape"?
-https://unsplash.com/photos/VrlNDUdnOGM
-https://unsplash.com/photos/r9RW20TrQ0Y
-https://unsplash.com/photos/lcm1eHlscKA
+- create icon at the top (address bar)
 
-DONE:
-
+# DONE:
 - The [MASSIVELY] logo should be changed to [home]
 - nav bar change on all files
 - Change the <title> tags
--- copy_contents.sh
+++ copy_contents.sh
@@ -8,6 +8,7 @@ else
 fi
 cp /home/ocean/Documents/GitHubDir/ITERCAD/video/rotate/EvenlyDistributed.mp4 contents/EvenlyDistributed.mp4
 cp /home/ocean/Documents/GitHubDir/ComplexAnalysis/exponentiate.pdf contents/exponentiate.pdf
+cp /home/ocean/Documents/GitHubDir/Chaos/simple_sweep.mp4 contents/.
 
 cp /home/ocean/Documents/Work/5.CV/tex/CV.pdf /home/ocean/Documents/Work/5.CV/tex/CV_short.pdf contents/CV/.
 cp /home/ocean/Documents/Work/5.CV/tex/CV.tex /home/ocean/Documents/Work/5.CV/tex/CV_short.tex contents/CV/. # add the source codes too
